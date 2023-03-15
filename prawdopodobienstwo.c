d09adf81a669e0d3afdfbba9371eec55cb9f6809
wercziq16 <120311552+wercziq16@users.noreply.github.com>
2023-03-15 23:14:40 +0100

    Create prawdopodobienstwo.c

---
+++ prawdopodobienstwo.c
@@ -0,0 +1,18 @@
+void wystapienia(FILE *in){
+  int znaki[255]={0};
+  double prawdopodobienstwo[255]={0};
+  int suma=0;
+  if (in!=NULL){
+    char znak;
+    while((znak=fgetc(in))!=EOF){
+      znaki[znak]++;
+      suma++;
+      } 
+  }
+  fclose(in);
+  for(int i=0; i<255; i++){
+    if(znaki[i]!=0){
+      prawdopodobienstwo[i]=((double)znaki[i]/(double)suma));
+      }
+  }
+}
