3c8ec2ac5d37e06927d16ee08436dfd9246cd2e0
wercziq16 <120311552+wercziq16@users.noreply.github.com>
2023-03-15 23:20:54 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "prawdopodobienstwo.h"
+
 int main(int argc, char** argv) {
   if(argc<2){
     printf("Nie podano pliku wejsciowego. Podaj plik w formacie txt.");
   }
   FILE *in = fopen(argv[1], "r");
-  int znaki[255]={0};
-  double prawdopodobienstwo[255]={0};
-  int suma=0;
-  if (in!=NULL){
-    char znak;
-    while((znak=fgetc(in))!=EOF){
-      znaki[znak]++;
-      suma++;
-      } 
-  }
-  fclose(in);
-  for(int i=0; i<255; i++){
-    if(znaki[i]!=0){
-      prawdopodobienstwo[i]=((double)znaki[i]/(double)suma);
-      }
-  }
+  wystapienia(in)
 
 }
