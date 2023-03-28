fb3041bbd6b88a3012d52b8104e64e3feca326f2
Dominika Kalinowska <kalinooovska@gmail.com>
2023-03-28 17:14:30 +0200

    typedef

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "prawdopodobienstwo.h"
+// #include "prawdopodobienstwo.h"
 
 int main(int argc, char** argv) {
-  if(argc<2){
+  if (argc<2) {
     printf("Nie podano pliku wejsciowego. Podaj plik w formacie txt.");
   }
   FILE *in = fopen(argv[1], "r");
-  wystapienia(in)
+  wystapienia(in);
 
 }
