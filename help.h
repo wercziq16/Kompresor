82b0b2776be40445eb8db7b65d476760fbd79e56
weronika <werainteria@gmail.com>
2023-04-07 15:50:23 +0200

    poprawki

--- help.h
+++ help.h
@@ -4,9 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char * help = 	"Pomoc programu kompresor/dekompresor\n\
-		 		Wywolanie:\n\
-		 	\t ./huff [infile] [outfile] [flagi]\n";
+
+char *help = "Pomoc programu kompresor/dekompresor\n"
+             "Wywołanie:\n"
+             "\t./huff [infile] [outfile] [flagi]\n"
+             "\tMożliwe flagi:\n"
+             "\t-0 brak kompresji\n"
+             "\t-1 kompresja 8-bitowa\n"
+             "\t-2 kompresja 12-bitowa\n"
+             "\t-3 kompresja 16-bitowa\n"
+             "\t-v statystyki kompresji\n"
+             "\t-d uruchomienie dekompresji\n";
 
 char * in_open = "Nie udalo sie otworzyc pliku wejsciowego\n";
 char * out_open = "Nie udalo sie otworzyc pliku wyjsciowego\n";
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> //do strcmp
 
+#include "suma.h"
 #include "help.h"
 #include "input.h"
 #include "huff.h"
@@ -9,27 +11,66 @@
 
 int main (int argc, char **argv)
 {
-	char * data = calloc(MAX_CHARS, sizeof(char));
+    	char * data = calloc(MAX_CHARS, sizeof(char));
 	unsigned int * frequency = calloc(MAX_CHARS, sizeof(int));
 
+	int compression_level = 0; // domyślnie brak kompresji
+    	int show_stats = 0; // domyślnie nie wyświetlamy statystyk
+    	int decompress = 0; // domyślnie tryb kompresji
+    
+   	 // przeszukiwanie argumentów
+    	for (int i = 3; i < argc; i++) {
+        	if (strcmp(argv[i], "-0") == 0) {
+            		compression_level = 0;
+        	} else if (strcmp(argv[i], "-1") == 0) {
+            		compression_level = 1;
+        	} else if (strcmp(argv[i], "-2") == 0) {
+            		compression_level = 2;
+       		} else if (strcmp(argv[i], "-3") == 0) {
+        	    	compression_level = 3;
+       		} else if (strcmp(argv[i], "-v") == 0) {
+           		 show_stats = 1;
+        	} else if (strcmp(argv[i], "-d") == 0) {
+           		 decompress = 1;
+       		} else {
+            		printf("Nieznana flaga: %s\n", argv[i]);
+            		exit(EXIT_FAILURE);
+       		 }
+  	  }	
+    
+	if (compression_level!=0 && decompress==1){
+		printf("Nie można jednocześnie skompresować i zdekompresować pliku.");
+		exit(EXIT_FAILURE);
+	}
+
+	if (show_stats==1 && decompress==1){
+		printf("Nie obsługujemy opcji pokazywania statystyk z dekompresji.");
+		exit(EXIT_FAILURE);
+	}
+	
 	if (argc < 3) {
 		print_help();
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
-		
+
+	if (strcmp(argv[1], argv[2])==0){
+		printf("Błąd. Podano ten sam plik na wejście i wyjście.");
+		exit(EXIT_FAILURE);
+	}
+
 	FILE * in = fopen(argv[2], "r");
 	FILE * out = fopen(argv[3], "w");
 
 	if (in == NULL) {
 		print_in_open();
 		print_help();
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	if (out == NULL) {
 		print_out_open();
 		print_help();
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	frequency = getFrequency(in);
@@ -52,4 +93,4 @@ int main (int argc, char **argv)
 	*/
 
 
-
+	
